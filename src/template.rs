9c1b845323700dbf02db6f706229566a1934cd60
486c <lopihavefun@gmail.com>
2023-12-10 13:18:24 +0300

    chore: added template

---
+++ src/template.rs
@@ -0,0 +1,22 @@
+use eyre::Result;
+
+pub fn run(input: &str) -> u64 {
+    let p2 = part2(input).unwrap();
+
+    p2
+}
+
+pub fn run_debug(input: &str) -> (u64, u64) {
+    let p1 = part1(input).unwrap();
+    let p2 = part2(input).unwrap();
+
+    (p1, p2)
+}
+
+pub fn part1(input: &str) -> Result<u64> {
+    todo!()
+}
+
+pub fn part2(input: &str) -> Result<u64> {
+    todo!()
+}
