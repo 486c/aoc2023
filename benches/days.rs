6abc5a4c6d6339f318104c58d580230c7b28d104
486c <lopihavefun@gmail.com>
2023-12-13 20:56:05 +0300

    feat: macros for generating benchmarks

--- benches/days.rs
+++ benches/days.rs
@@ -1,46 +1,46 @@
 use divan::{black_box, Bencher};
 
+macro_rules! generate_part {
+    ($day:ident, $part:ident) => {
+        #[divan::bench]
+        fn $part(bencher: divan::Bencher) {
+            let mut $day = String::new();
+
+            let input_path = format!(
+                "inputs/{}_input.txt", 
+                stringify!($day)
+            );
+
+            File::open(&input_path).unwrap()
+                .read_to_string(&mut $day).unwrap();
+
+            bencher
+                .with_inputs(|| {
+                    $day.as_str()
+                })
+                .bench_local_refs(|input| {
+                    aoc::$day::$part(input)
+                });
+        }
+    }
+}
+
+macro_rules! generate_bench {
+    ($day:ident) => {
+        mod $day {
+            use divan::{black_box, Bencher};
+            use std::fs::File;
+            use std::io::Read;
+
+            generate_part!($day, part1);
+            generate_part!($day, part2);
+        }
+    }
+}
+
+generate_bench!(day1);
+generate_bench!(day2);
+
 fn main() {
     divan::main();
 }
-
-mod day1 {
-    use divan::{black_box, Bencher};
-    use std::fs::File;
-    use std::io::Read;
-
-    #[divan::bench]
-    fn part1(bencher: divan::Bencher) {
-        let mut day1 = String::new();
-
-        File::open("inputs/day1_input.txt").unwrap()
-            .read_to_string(&mut day1).unwrap();
-
-        bencher
-            .with_inputs(|| {
-                day1.as_str()
-            })
-            .bench_local_refs(|input| {
-                aoc::day1::part1(input)
-            });
-
-    }
-
-    #[divan::bench]
-    fn part2(bencher: divan::Bencher) {
-        let mut day1 = String::new();
-
-        File::open("inputs/day1_input.txt").unwrap()
-            .read_to_string(&mut day1).unwrap();
-
-        bencher
-            .with_inputs(|| {
-                day1.as_str()
-            })
-            .bench_local_refs(|input| {
-                aoc::day1::part2(input)
-            });
-
-    }
-}
-
