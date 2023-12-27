ff3f88ef353be3342c24c4f5f62e1f7bf3d8a903
486c <lopihavefun@gmail.com>
2023-12-27 14:57:56 +0300

    day6(zig): part 2 complete

--- zig/src/day6.zig
+++ zig/src/day6.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
 
-pub fn calc_dist(hold: u32, lasts: u32) u32 {
+pub fn calc_dist(hold: u64, lasts: u64) u64 {
     return hold * (lasts - hold);
 }
 
@@ -53,9 +53,55 @@ pub fn part1(input: []const u8) u64 {
 }
 
 pub fn part2(input: []const u8) u64 {
-    _ = input;
+    var lines = std.mem.splitAny(u8, input, "\n");
 
-    return 0;
+    var time_num: u64 = 0;
+    var dist_num: u64 = 0;
+
+    const time_raw = lines.next().?;
+    const distance = lines.next().?;
+
+    var time_split = std.mem.splitAny(u8, time_raw[9..], " ");
+    var dist_split = std.mem.splitAny(u8, distance[9..], " ");
+
+    while (time_split.next()) |l_time| {
+        if (l_time.len == 0) continue;
+        const digit = std.fmt.parseInt(u32, l_time, 10) catch continue;
+        const mult: u32 = switch (l_time.len) {
+            1 => 10,
+            2 => 100,
+            3 => 1000,
+            4 => 10000,
+            else => unreachable,
+        };
+        time_num = time_num * mult + digit;
+    }
+
+    while (dist_split.next()) |l_dist| {
+        if (l_dist.len == 0) continue;
+
+        const digit = std.fmt.parseInt(u32, l_dist, 10) catch continue;
+        const mult: u32 = switch (l_dist.len) {
+            1 => 10,
+            2 => 100,
+            3 => 1000,
+            4 => 10000,
+            else => unreachable,
+        };
+
+        dist_num = dist_num * mult + digit;
+    }
+
+    var win_amount: u64 = 0;
+    for (0..time_num) |hold| {
+        const result_dist = calc_dist(@intCast(hold), @intCast(time_num));
+
+        if (result_dist > dist_num) {
+            win_amount += 1;
+        }
+    }
+
+    return win_amount;
 }
 
 pub fn run() !void {
@@ -67,7 +113,11 @@ pub fn run() !void {
 
     const p1_time: f64 = @as(f64, @floatFromInt(timer.read())) / 1e6;
 
+    timer = try std.time.Timer.start();
     const res_p2 = part2(buff);
+    timer.reset();
 
-    std.debug.print("Results: {d} ({d}ms) | {d}\n", .{ res_p1, p1_time, res_p2 });
+    const p2_time: f64 = @as(f64, @floatFromInt(timer.read())) / 1e6;
+
+    std.debug.print("Results: {d} ({d}ms) | {d} ({d}ms)\n", .{ res_p1, p1_time, res_p2, p2_time });
 }
