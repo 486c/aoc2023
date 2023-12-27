8ce2d552cad7f0deb350cdf883445c4acfb1ccb7
486c <lopihavefun@gmail.com>
2023-12-27 13:05:49 +0300

    feat: zig day 1 part 2 complete

--- zig/src/day1.zig
+++ zig/src/day1.zig
@@ -30,9 +30,63 @@ pub fn part1(input: []const u8) u64 {
     return sum;
 }
 
+const KeyValue = struct { key: []const u8, value: u8 };
+
 pub fn part2(input: []const u8) u64 {
-    _ = input;
-    return 0;
+    const values = [_]KeyValue{
+        KeyValue{ .key = "one", .value = 1 },
+        KeyValue{ .key = "two", .value = 2 },
+        KeyValue{ .key = "three", .value = 3 },
+        KeyValue{ .key = "four", .value = 4 },
+        KeyValue{ .key = "five", .value = 5 },
+        KeyValue{ .key = "six", .value = 6 },
+        KeyValue{ .key = "seven", .value = 7 },
+        KeyValue{ .key = "eight", .value = 8 },
+        KeyValue{ .key = "nine", .value = 9 },
+    };
+
+    var sum: u64 = 0;
+
+    var first_digit: u8 = 0;
+    var last_digit: u8 = 0;
+
+    var lines = std.mem.splitAny(u8, input, "\n");
+    while (lines.next()) |line| {
+        var n: usize = 0;
+        first_digit = 0;
+        last_digit = 0;
+        char_loop: while (n < line.len) : (n += 1) {
+            for (values) |kv| {
+                if (std.mem.startsWith(u8, line[n..], kv.key)) {
+                    if (first_digit == 0) {
+                        first_digit = kv.value;
+                    } else {
+                        last_digit = kv.value;
+                    }
+
+                    n += kv.key.len - 1;
+                    continue :char_loop;
+                }
+            }
+
+            const d = std.fmt.charToDigit(line[n], 10) catch continue;
+
+            if (first_digit == 0) {
+                first_digit = d;
+            } else {
+                last_digit = d;
+            }
+        }
+
+        if (last_digit == 0) last_digit = first_digit;
+
+        const num = first_digit * 10 + last_digit;
+
+        print("{d}\n", .{num});
+        sum += num;
+    }
+
+    return sum;
 }
 
 pub fn run() !void {
@@ -40,7 +94,7 @@ pub fn run() !void {
 
     //const buff = try std.fs.cwd()
     //.readFileAlloc(alloc.allocator(), "inputs/day1_input.txt", std.math.maxInt(usize));
-    const buff = @embedFile("./inputs/day1_input.txt");
+    const buff = @embedFile("./inputs/day1_test.txt");
 
     var timer = try std.time.Timer.start();
     const res_p1 = part1(buff);
